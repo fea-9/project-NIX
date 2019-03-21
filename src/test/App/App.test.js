a2a6f25b29c658c045d81888e11ed8d841e988c2
VieronikaHontar <ronnicka@gmail.com>
2019-03-21 23:27:19 +0200

    new structure +

--- src/test/App/App.test.js
+++ src/test/App/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Component } from 'react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       Hello
+      </div>
+    );
+  }
+}
+
+export default App;
