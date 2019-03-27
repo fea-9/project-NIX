89d19f2f0b331a62175143fdec2799efdf37337c
Silent <tykhyi.andrii@gmail.com>
2019-03-28 01:34:17 +0200

    added pages templates

---
+++ src/components/Pages/ProfilePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {PageTemplate} from '../Templates/PageTemplate';
+//import components profile, header, sidebar
+
+
+// class ProfilePage extends Component {
+
+//     componentDidMount() {
+
+//     }
+
+//     render() {
+//         return (
+//             <PageTemplate sidebar={} header={} content={} />
+//         )
+//     }
+// }
+
+// export default ProfilePage;
