b1b5426f9f5cbabd420d0ca84500b97f78be1b49
Emiro512 <zirenko.a.s@gmail.com>
2019-03-31 21:20:17 +0300

    Icons Commit

---
+++ src/components/BaseComponents/Icons/IconsSearch.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export const SearchClearIcon = props => (
+    <svg
+        className ="search-clear-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20">
+        <defs>
+            <clipPath id="search-clear-icon-clip">
+                <rect class="search-clear-icon-clip" width="8.333" height="8.333"/>
+            </clipPath>
+        </defs>
+        <g transform="translate(-1165 -304)">
+            <rect className="search-clear-icon-round"
+                  width="20"
+                  height="20"
+                  rx="10"
+                  transform="translate(1165 304)"
+            />
+        <g transform="translate(1170.893 309.893)">
+            <g class="search-clear-icon-clip">
+                <path
+                      d="M4.836,4.015,8.071.853a.51.51,0,0,0,0-.712.54.54,0,0,0-.728,0L4.108,3.3.873.141a.54.54,0,0,0-.728,0,.51.51,0,0,0,0,.712L3.379,4.015.145,7.177a.51.51,0,0,0,0,.712.508.508,0,0,0,.364.147.508.508,0,0,0,.364-.147L4.108,4.727,7.343,7.888a.509.509,0,1,0,.728-.712Zm0,0"
+                      transform="translate(0.005 0.11)"
+                /></g>
+        </g>
+    </g>
+    </svg>
+);
+
+export const ArrowDownSearchIcon = props => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="8"
+        height="5"
+        viewBox="0 0 8 5">
+        <defs>
+            <clipPath id="adsi1">
+                <path
+                    class="adsi1"
+                    d="M253.635-231.009l3.1-3.062-3.1-3.062.949-.938,4.051,4-4.051,4Z"
+                    transform="translate(0 0)"
+                />
+            </clipPath>
+            <clipPath id="adsi2">
+                <path class="adsi2"
+                      d="M-254,50.795H535.593V-534H-254Z"
+                      transform="translate(254 534)"/>
+            </clipPath>
+        </defs>
+        <g className="adsi3"
+           transform="translate(-230.072 -253.635) rotate(90)"
+        >
+            <g
+                className="adsi4"
+               transform="translate(-247.396 -526.501)"
+            >
+                <path
+                    className="adsi2"
+                    d="M248.635-225.325h14.87v-17.747h-14.87Z"
+                    transform="translate(247.461 526.627)"
+                />
+            </g>
+        </g>
+    </svg>
+);
