5ad168bea36ba117543ab0747a8bb31734e49b3a
Silent <tykhyi.andrii@gmail.com>
2019-04-01 15:31:50 +0300

    fixed routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+
 import AuthPage from "../Pages/AuthPage";
-import { NavList } from "../Nav/NavList";
-import { Social } from "../Social/Social";
-import Sidebar from "../Sidebar/Sidebar";
+import CommunityPage from "../Pages/CommunityPage";
+import DashboardPage from "../Pages/DashboardPage";
+import DocumentsPage from "../Pages/DocumentsPage";
+import ProfilePage from "../Pages/ProfilePage";
+import SearchPage from "../Pages/SearchPage";
 
 class App extends Component {
   componentDidMount() {
     // const token = localStorage.getItem("token");
-
     // if (token) {
     //   const { setToken } = this.props;
     //   return setToken(token);
@@ -18,12 +20,12 @@ class App extends Component {
   render() {
     return (
       <Switch>
-      <Route exact path="/auth/:name" component={AuthPage} />
-      <Route exact path="/dashboard" component={NavList} />
-      <Route exact path="/documents" component={Social} />
-      <Route exact path='/profile/' component={Sidebar} />
-      <Route exact path='/community/:user' component={Sidebar} />
-      <Route exact path='/search' component={Sidebar} />
+        <Route exact path="/auth/:name" component={AuthPage} />
+        <Route exact path="/dashboard" component={DashboardPage} />
+        <Route exact path="/documents" component={DocumentsPage} />
+        <Route exact path="/profile/" component={ProfilePage} />
+        <Route exact path="/community/:user" component={CommunityPage} />
+        <Route exact path="/search" component={SearchPage} />
       </Switch>
     );
   }
