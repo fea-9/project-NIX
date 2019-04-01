33e69ea41c6762d74557d11cae649197bef7628c
Silent <tykhyi.andrii@gmail.com>
2019-04-01 15:30:58 +0300

    Add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import AuthPage from '../Pages/AuthPage';
-
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import AuthPage from "../Pages/AuthPage";
+import { NavList } from "../Nav/NavList";
+import { Social } from "../Social/Social";
+import Sidebar from "../Sidebar/Sidebar";
 
 class App extends Component {
-
   componentDidMount() {
-    const token = localStorage.getItem("token");
+    // const token = localStorage.getItem("token");
 
-		if (token) {
-			const { setToken } = this.props;
-			return setToken(token);
-		}
+    // if (token) {
+    //   const { setToken } = this.props;
+    //   return setToken(token);
+    // }
   }
 
   render() {
     return (
       <Switch>
-        <Route path='/auth' component={AuthPage}/>
+      <Route exact path="/auth/:name" component={AuthPage} />
+      <Route exact path="/dashboard" component={NavList} />
+      <Route exact path="/documents" component={Social} />
+      <Route exact path='/profile/' component={Sidebar} />
+      <Route exact path='/community/:user' component={Sidebar} />
+      <Route exact path='/search' component={Sidebar} />
       </Switch>
     );
   }
