f106da73b5872ec9acf1b046467adeedda262ee5
VieronikaHontar <ronnicka@gmail.com>
2019-04-01 09:50:36 +0300

    profile header started

---
+++ src/components/Profile/ProfileHeader.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+
+import { connect } from 'react-redux';
+
+class ProfileHeader extends Component {
+    getPeiodOfMembership = dateOfRegistration => {
+        let diff = Math.floor(dateOfRegistration - new Date().getTime());
+        let day = 1000 * 60 * 60 * 24;
+    
+        let days = Math.floor(diff/day);
+        let months = Math.floor(days/31);
+        let years = Math.floor(months/12);
+        
+        let message = "Member for "
+        if (months > 12) {
+            message += `${years} years`
+        } else if (months < 12 && months > 1) {
+            message += `${months} months`
+        } else if (days < 31 && days > 1) {
+            message += `${days} days`
+        } else if (days < 1){
+            message += "1 day"
+        } 
+
+        return message
+    }
+
+    render () {
+        const {fullName, created_at} = this.props
+        return (
+            <>
+                <h2>
+                    {fullName}
+                </h2>
+                <p>
+                    {getPeiodOfMembership(created_at)}
+                </p>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+	return {        
+        fullName: state.auth.user.fullName,
+        created_at: state.auth.user.created_at
+    };
+};
+
+export default connect(mapStateToProps, null)(ProfileHeader);
