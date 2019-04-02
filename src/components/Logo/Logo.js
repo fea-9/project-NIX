35b36f5853875115a377b59f0f9b23b22c84922d
Silent <tykhyi.andrii@gmail.com>
2019-04-02 19:24:42 +0300

    edded work with props

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-export const Logo = props => (
-  <Link to="/dashboard" className='link'>
-    <h1 className="logo">{props.minimized ? "L" : "Logo"}</h1>
-  </Link>
+export const Logo = ({minimized, text = "Logo", ...props}) => (
+  <h1 {...props}>{minimized ? "L" : {text}}</h1>
 );
