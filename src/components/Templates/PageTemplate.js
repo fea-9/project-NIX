90b095f1a5fa4011c5c00e356c8e7e756cb4953c
Silent <tykhyi.andrii@gmail.com>
2019-04-02 19:03:53 +0300

    refactoring

--- src/components/Templates/PageTemplate.js
+++ src/components/Templates/PageTemplate.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Helmet} from 'react-helmet';
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
-export const PageTemplate = props => {
-  const {header, sidebar, content} = props;
-
-  return (
-    <React.Fragment>
-      <Helmet
-        title="test"
-        titleTemplate="MySite.com - %s"
-        defaultTitle="Dashboard"
-      />
-      <div className="wrapper">
-        <main className="main">
-          {sidebar && <aside className="main__sidebar">{sidebar}</aside>}
-          <div className="main__container">
-            {header &&
-              <header className="main__container__header">{header}</header>}
-            <div className="main__container__content">{content}</div>
-          </div>
-        </main>
-      </div>
-    </React.Fragment>
-  );
-};
+export const PageTemplate = ({ header, sidebar, content }) => (
+  <React.Fragment>
+    <Helmet
+      title="test"
+      titleTemplate="MySite.com - %s"
+      defaultTitle="Dashboard"
+    />
+    <div className="wrapper">
+      <main className="main">
+        {sidebar && <aside className="main__sidebar">{sidebar}</aside>}
+        <div className="main__container">
+          {header && (
+            <header className="main__container__header">{header}</header>
+          )}
+          <div className="main__container__content">{content}</div>
+        </div>
+      </main>
+    </div>
+  </React.Fragment>
+);
 
 PageTemplate.propTypes = {
   header: PropTypes.element,
