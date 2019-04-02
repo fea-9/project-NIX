0f92db579d0ab99a7152131a5ac048d4917c5f51
Silent <tykhyi.andrii@gmail.com>
2019-04-02 16:31:37 +0300

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import * as actions from "../actions/token";
 
 import AuthPage from "../Pages/AuthPage";
 import CommunityPage from "../Pages/CommunityPage";
@@ -10,17 +13,23 @@ import SearchPage from "../Pages/SearchPage";
 
 class App extends Component {
   componentDidMount() {
-    // const token = localStorage.getItem("token");
-    // if (token) {
-    //   const { setToken } = this.props;
-    //   return setToken(token);
-    // }
+    const token = localStorage.getItem("access_token");
+    const refreshToken = localStorage.getItem("refresh_token");
+
+    console.log('token', token)
+    console.log('refreshToken', refreshToken)
+
+    if (token && refreshToken) {
+      const { refreshTokens } = this.props;
+      return refreshTokens({refresh_token: refreshToken});
+    }
+    console.log("this.props", this.props);
   }
 
   render() {
     return (
       <Switch>
-        <Route path="/auth" component={AuthPage} />
+        <Route path="/auth/:id" component={AuthPage} />
         <Route exact path="/profile" component={ProfilePage} />
         <Route exact path="/search" component={SearchPage} />
         <Route exact path="/dashboard" component={DashboardPage} />
@@ -30,8 +39,15 @@ class App extends Component {
 
         <Route render={() => <div>404</div>} />
       </Switch>
-    )
+    );
   }
 }
 
-export default App;
+const mapDispatchToProps = { ...actions };
+
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(App)
+);
