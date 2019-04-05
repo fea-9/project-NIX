a4c9e0448011e84294d58132c04a99e48f8758e3
Silent <tykhyi.andrii@gmail.com>
2019-04-05 23:53:58 +0300

    added fake props for initial start

--- src/components/Pages/CommunityPage.js
+++ src/components/Pages/CommunityPage.js
@@ -3,13 +3,19 @@ import React, { Component } from "react";
 import { PageTemplate } from "../Templates/PageTemplate";
 
 import Sidebar from "../Sidebar/Sidebar";
-import CommunityTable from "../CommunityTable/CommunityTable";
+// import CommunityTable from "../CommunityTable/CommunityTable";
 
 class CommunityPage extends Component {
   componentDidMount() {}
 
   render() {
-    return <PageTemplate sidebar={<Sidebar />} content={<CommunityTable />} />;
+    return (
+      <PageTemplate
+        sidebar={<Sidebar />}
+        // content={<CommunityTable />}
+        content={React.createElement("div")}
+      />
+    );
   }
 }
 
