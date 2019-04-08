f7c4b4d6f15b01255bf85c5ba4fba7bcbe76b88f
Silent <tykhyi.andrii@gmail.com>
2019-04-08 18:23:45 +0300

    add sidebar reducer

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import auth from "./auth";
 import resize from "./resize";
+import sidebar from "./sidebar";
 
 export default combineReducers({
+  sidebar,
   resize,
   auth
 });
