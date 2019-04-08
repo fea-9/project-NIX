1d701f04e21eb37070f80b9dd1a2db425fd70abd
Silent <tykhyi.andrii@gmail.com>
2019-04-08 18:31:00 +0300

    new structure

--- src/components/Nav/NavListItem.js
+++ src/components/Nav/NavListItem.js
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-export const NavListItem = props => (
-  <NavLink className="link" to={props.path}>
-    {props.icon}
-    {props.text}
-  </NavLink>
+export const NavListItem = ({ path, elem, icon, text, minimized }) => (
+  <li
+    className={
+      minimized ? "nav-list__item nav-list__item_minimized" : "nav-list__item"
+    }
+  >
+    <NavLink className="link-sidebar" to={path}>
+      {elem && <div className="link-sidebar__profile">{elem}</div>}
+      {icon}
+      {<span className="link-sidebar__text">{text}</span>}
+    </NavLink>
+  </li>
 );
