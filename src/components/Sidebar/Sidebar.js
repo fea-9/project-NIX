2e7d6d43907c1330b13b8fe0bfc9fabe8a59fb1d
Silent <tykhyi.andrii@gmail.com>
2019-04-08 18:31:11 +0300

    remove state

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -8,55 +8,39 @@ import { Button } from "../BaseComponents/Forms/Button";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "../actions/auth";
+import { sidebarSize } from "../actions/sidebar";
 
 class Sidebar extends Component {
-  state = {
-    minimized: false
-  };
-
-  clickHandler = e => {
-    this.setState(state => ({
-      minimized: !state.minimized
-    }));
-  };
-
   render() {
-    const { authLogout } = this.props;
+    const { authLogout, minimized, sidebarSize, mobile } = this.props;
 
     return (
       <div
         className={
-          this.state.minimized
+          minimized
             ? "sidebar-wrapper sidebar-wrapper_minimized"
             : "sidebar-wrapper"
         }
       >
         <div className="top-content-wrapper">
-          <div className="logo-wrapper">
-            <Logo className="logo logo_sidebar" minimized={this.state.minimized} />
-          </div>
-          <div className="user-profile">
-            <div className="profile" onClick={this.clickHandler} />
-          </div>
-          <div className="nav">
-            <NavList minimized={this.state.minimized} />
-          </div>
+          <Logo className="logo logo_sidebar" minimized={minimized} button={mobile}/>
+          <NavList minimized={minimized} />
         </div>
-        <div className="social">
-          <Social minimized={this.state.minimized} />
-        </div>
-        <div className="logout">
-          <Button className="logout-btn" text="Log out" onClick={authLogout} />
+        <div className="bottom-content-wrapper">
+          <Social minimized={minimized} />
+          <Button className="logout-btn" text="Log out" onClick={sidebarSize} />
         </div>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ ...actions }, dispatch);
+const mapStateToProps = state => ({
+  minimized: state.sidebar.minimized,
+  mobile: state.resize.mobile
+});
 
 export default connect(
-  null,
-  mapDispatchToProps
+  mapStateToProps,
+  { ...actions, sidebarSize }
 )(Sidebar);
