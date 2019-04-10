ebe12e0fcf682a81586f307c5d8b76941ad76719
Silent <tykhyi.andrii@gmail.com>
2019-04-10 16:18:30 +0300

    added onclick on btn show menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { sidebarSize } from "../actions/sidebar";
 //import * as Icons from '../BaseComponents/Icons/IconsHeader'
-import Button from '../BaseComponents/Button'
-import Icon from '../BaseComponents/icon/index';
+import Button from "../BaseComponents/Button";
+import Icon from "../BaseComponents/icon/index";
 
-export const HeaderTemplate = ({title, details, component, ...rest}) => (
-    <>
-        <Button className = "header__icon-box"><Icon className = "burger-menu-icon" type = "burgerMenuIcon" width = {20} height = {20} viewBox="0 0 20 20"/></Button>
-        <div className = "header__title-box" >
-            { title && <h2 className = "header__name" >
-                {title}
-            </h2>}
-            { details && <p className = "header__details" >
-                {details}
-            </p>}
-            
-        </div>
-        {component && 
-            <div className = "header__component-box" >
-                {component}
-            </div>   } 
+const Header = ({ title, details, component, sidebarSize, ...rest }) => (
+  <>
+    <Button className="header__icon-box" onClick={sidebarSize}>
+      <Icon
+        className="burger-menu-icon"
+        type="burgerMenuIcon"
+        width={20}
+        height={20}
+        viewBox="0 0 20 20"
+      />
+    </Button>
+    <div className="header__title-box">
+      {title && <h2 className="header__name">{title}</h2>}
+      {details && <p className="header__details">{details}</p>}
+    </div>
+    {component && <div className="header__component-box">{component}</div>}
+  </>
+);
 
-        
-    </>
-)
+export const HeaderTemplate = connect(
+  null,
+  { sidebarSize }
+)(Header);
