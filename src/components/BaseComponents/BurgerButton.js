238063acb90d366d3f5ae6e80b096add27560d42
Silent <tykhyi.andrii@gmail.com>
2019-04-11 14:25:22 +0300

    fixed props

--- src/components/BaseComponents/BurgerButton.js
+++ src/components/BaseComponents/BurgerButton.js
@@ -1,31 +1,18 @@
-import React, {PureComponent} from 'react'
-import Button from './Button'
-import Icon from "./icon"
+import React from "react";
+import Button from "./Button";
+import Icon from "./icon";
 
-class BurgerButton extends PureComponent{
-    state = {
-            sbIsOpen:false    
-        }
-    
-    changeBurger(){
-        this.setState( {sbIsOpen: !this.state.sbIsOpen})
-    }
-
-    render(){
-        
-        if(this.state.sbIsOpen === true){
-            return(
-                <Button onClick={() => this.changeBurger()}className="btn-close" children="X"/>
-            )
-        }
-        else{
-            return(
-                <Button onClick={() => this.changeBurger()} className="btn-burger">
-                <Icon className="burger-menu-btn-mobile" type="burgerMenuIcon" width={15} height={16} viewBox="0 0 14 16"/>
-                </Button>
-            )
-        }
-    }
-}
-
-export default BurgerButton
+export const BurgerButton = ({ isOpen, onClick }) =>
+  isOpen ? (
+    <Button onClick={onClick} className="btn-close" children="X" />
+  ) : (
+    <Button onClick={onClick} className="btn-burger">
+      <Icon
+        className="burger-menu-btn-mobile"
+        type="burgerMenuIcon"
+        width={15}
+        height={16}
+        viewBox="0 0 14 16"
+      />
+    </Button>
+  );
