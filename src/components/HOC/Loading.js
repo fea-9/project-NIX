cbb93a7c581d9023a68256c75020492909365571
Silent <tykhyi.andrii@gmail.com>
2019-04-15 17:21:59 +0300

    added test for HOC Loading

--- src/components/HOC/Loading.js
+++ src/components/HOC/Loading.js
@@ -1,4 +1,11 @@
 import React from "react";
 import Spinner from "../Spinner/Spinner";
 
-export default ({ flag, children }) => (flag ? <Spinner /> : children);
+import PropTypes from "prop-types";
+
+export const Loading = ({ flag, children }) => (flag ? <Spinner /> : children);
+
+Loading.propTypes = {
+  flag: PropTypes.bool,
+  children: PropTypes.element
+};
