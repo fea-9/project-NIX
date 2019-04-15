0f92fed11b015279b9390933d32440e1cb202117
Silent <tykhyi.andrii@gmail.com>
2019-04-15 19:42:49 +0300

    added utils parse jwt and test

---
+++ src/__tests__/utils/parseJwt.test.js
@@ -0,0 +1,20 @@
+import { parseJwt } from "../../utils/parseJwt";
+
+describe("parseJwt", () => {
+  const jwt =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiMWE0MGM0NmMtZjA2OS00NzY0LWFjMzctNTE0NzIyOWNlNmU1IiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJyb2xlSWQiOjF9LCJpYXQiOjE1NTM3MzEzMzYsImV4cCI6MTU1MzczNDkzNn0.Fsw6c9_JooRFyAZJ4jJjZ9-ZCcUJP4f6VsdwdAdG34M";
+
+  const result = {
+    user: {
+      id: "1a40c46c-f069-4764-ac37-5147229ce6e5",
+      email: "admin@admin.com",
+      roleId: 1
+    },
+    iat: 1553731336,
+    exp: 1553734936
+  };
+
+  it("returned object from after parse jwt", () => {
+    expect(parseJwt(jwt)).toEqual(result);
+  });
+});
