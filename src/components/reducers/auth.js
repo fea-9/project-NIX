1f9e685f8db615993332fe31a0550ee47f225f45
Silent <tykhyi.andrii@gmail.com>
2019-04-20 14:21:30 +0300

    deleted key "message" in success cases

--- src/components/reducers/auth.js
+++ src/components/reducers/auth.js
@@ -4,29 +4,48 @@ export default (state = initialState.auth, { type, payload, error }) => {
   switch (type) {
     case "AUTH_REQUEST": {
       return { ...state, isFetching: true };
-    }    
+    }
     case "AUTH_REQUEST_SUCCESS": {
-      const { Item, message } = payload;
-      return { ...state, error: false, user: Item, message, isFetching: false };
+      const { Item } = payload;
+      return {
+        ...state,
+        error: false,
+        user: Item,
+        message: null,
+        isFetching: false
+      };
     }
     case "AUTH_REQUEST_FAIL": {
-      const { message } = error.response.data;
-      return { ...initialState.auth, error: true, message, isFetching: false };
+      return {
+        ...state,
+        error: true,
+        message: error.response.data.message,
+        isFetching: false
+      };
     }
     case "AUTH_RESET": {
       return { ...initialState.auth, isFetching: false };
-    }    
+    }
 
     case "AUTH_UPDATE_REQUEST": {
       return { ...state, isFetching: true };
     }
     case "AUTH_UPDATE_REQUEST_SUCCESS": {
-      const { message } = payload;
-      return { ...state, error: false, user: payload, message, isFetching: false };
+      return {
+        ...state,
+        error: false,
+        user: payload,
+        message: null,
+        isFetching: false
+      };
     }
     case "AUTH_UPDATE_REQUEST_FAIL": {
-      const { message } = error.response.data;
-      return { ...state, error: true, message, isFetching: false };
+      return {
+        ...state,
+        error: true,
+        message: error.response.data.message,
+        isFetching: false
+      };
     }
 
     case "TOKEN_REFRESH_REQUEST": {
@@ -37,10 +56,9 @@ export default (state = initialState.auth, { type, payload, error }) => {
       return { ...state, error: false, user: Item, isFetching_token: false };
     }
     case "TOKEN_REFRESH_REQUEST_FAIL": {
-      const { err } = error;
-      return { ...initialState.auth, error: err, isFetching_token: false };
+      return { ...initialState.auth, error, isFetching_token: false };
     }
-    
+
     case "INTERCEPTOR_WORKING_SET": {
       return { ...state, interseptrorWorking: payload };
     }
