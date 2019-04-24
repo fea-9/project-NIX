3dd0d2d46e29b66e1b30596ce9385fa7fe441967
Silent <tykhyi.andrii@gmail.com>
2019-04-24 14:23:51 +0300

    added spread in render

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-export const Copyright = () => {
-  const year = new Date().getFullYear();
+import { PropTypes } from "prop-types";
 
-  return (
-    <div className="copyright">
-      Copyright &copy; {year}.&nbsp;
-      <a className="copyright__link" href="http://www.google.com.ua">
-        Cookie Preferences
-      </a>
-      ,&nbsp;
-      <a className="copyright__link" href="http://www.google.com.ua">
-        Privacy
-      </a>
-      &nbsp;and&nbsp;
-      <a className="copyright__link" href="http://www.google.com.ua">
-        Terms
-      </a>
-    </div>
-  );
-};
+export class Copyright extends Component {
+  static propTypes = {
+    searchClass: PropTypes.bool
+  };
+
+  state = {
+    year: new Date().getFullYear()
+  };
+
+  render() {
+    const { year } = this.state;
+    const { searchClass } = this.props;
+
+    return (
+      <div className={searchClass ? "copyright copyright_serach" : "copyright"}>
+        Copyright &copy; {year}.&nbsp;
+        <a className="copyright__link" href="http://www.google.com.ua">
+          Cookie Preferences
+        </a>
+        ,&nbsp;
+        <a className="copyright__link" href="http://www.google.com.ua">
+          Privacy
+        </a>
+        &nbsp;and&nbsp;
+        <a className="copyright__link" href="http://www.google.com.ua">
+          Terms
+        </a>
+      </div>
+    );
+  }
+}
